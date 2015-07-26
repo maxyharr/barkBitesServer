05656ae07ce89d64e084ef61d2af8105adfbe07f
Max Harris <mjharris@colorado.edu>
2015-07-26 15:09:48 -0600

    connects mongo through mongoose using mongolab free database

--- app.js
+++ app.js
@@ -1,5 +1,23 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+// Mongo
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://mharris7190:J)#nd0a@ds059712.mongolab.com:59712/barkbites');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000;
+
+// ROUTES FOR OUR API
+// =============================================================================
+var router = express.Router();              // get an instance of the express Router
+
+
 
 app.get('/location/:lat/:long', function (req, res) {
   var obj = {'location': {
@@ -9,9 +27,8 @@ app.get('/location/:lat/:long', function (req, res) {
   res.send(obj);
 });
 
-var server = app.listen(3000, function () {
+var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
+  console.log('BarkBitesServer listening at http://%s:%s', host, port);
 });
