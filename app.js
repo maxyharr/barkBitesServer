67324793f778cc7b54a711016280ff2ea8f34ae9
Max Harris <mjharris@colorado.edu>
2015-07-26 18:03:58 -0600

    adds places api for getting places in our database

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var router = express.Router();
+router.use(function (req, res, next) { next(); });
 
 // Mongo && models
 var mongoose = require('mongoose');
@@ -12,23 +14,57 @@ mongoose.connect('mongodb://bbuser:jjJ#(*sD#59jskdL@ds059712.mongolab.com:59712/
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-var port = process.env.PORT || 3000;
+// Models
+var Place = mongoose.model('Place');
 
-// ROUTES FOR OUR API
-// =============================================================================
-var router = express.Router();              // get an instance of the express Router
+// API
+router.route('/places')
+  .get(function (req, res) {
+    Place.find(function (err, places) {
+      if (err) { res.sendStatus(err); }
+      res.json(places);
+    });
+  })
 
-app.get('/location/:lat/:long', function (req, res) {
-  var obj = 
-    { 
-      'location': {
-        'latitude': req.params.lat,
-        'longitude': req.params.long
-      }
+  .post(function (req, res) {
+    var place = Place.findOne({"googlePlaceId": req.body.googlePlaceId});
+    if (!place) {
+      Place.collection.insert(req.body, function (err) {
+        if (err) res.sendStatus(err);
+        res.json({message: 'Place created!'});
+      });
+    } else {
+      res.json({message: 'Place already created!'});
     }
-  res.send(obj);
-});
+  });
 
+router.route('/places/:id')
+  .get(function (req, res) {
+    Place.findOne(function (err, place) {
+      if (err) { res.sendStatus(err); }
+      res.json(place);
+    });
+  })
+
+  .delete(function (req, res) {
+    Place.remove({ _id: req.params.id }, function (err) {
+      if (err) res.sendStatus(err);
+      res.sendStatus(204);
+    });
+  })
+
+  .put(function (req, res) {
+    Place.findOneAndUpdate({_id: req.params.id}, req.body, function (err, place) {
+      if (err) {
+        res.sendStatus(err);
+      } else {
+        res.json(place);
+      }
+    });
+  })
+
+app.use('/api/v0', router);
+var port = process.env.PORT || 3000;
 var server = app.listen(port, function () {
   var host = server.address().address;
   var port = server.address().port;
